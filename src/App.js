5e3d7efbe8941da778994377d61736ab3210660c
Jason Christopher <jchristopher2448@gmail.com>
2022-11-08 12:46:47 -0600

    fixed issue

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import Header from './Header.js';
-import Main from './Main.js';
-import Footer from './Footer.js';
+import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Header/>
-        <Main/>
-        <Footer/>
-      </>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
