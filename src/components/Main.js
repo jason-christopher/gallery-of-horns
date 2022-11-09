7a54aaa12c22c36bb892e82a53d25d8c272d7d23
Jason Christopher <jchristopher2448@gmail.com>
2022-11-09 09:54:04 -0600

    changed forEach to map in Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,18 +7,15 @@ console.log(data);
 class Main extends React.Component {
   render() {
 
-    let beastArray = [];
-    data.forEach(beast => {
-      beastArray.push(
-        <HornedBeast
-          key = {beast._id}
-          title = {beast.title}
-          imageUrl = {beast.image_url}
-          description = {beast.description}
-          keyword = {beast.keyword}
-          horns = {beast.horns}
-        />
-      )
+    let beastArray = data.map(beast => {
+      return <HornedBeast
+        key = {beast._id}
+        title = {beast.title}
+        imageUrl = {beast.image_url}
+        description = {beast.description}
+        keyword = {beast.keyword}
+        horns = {beast.horns}
+      />
     });
 
     return (
