cee9177ff000239f24245ca0fee276458762e3db
Jason Christopher <jchristopher2448@gmail.com>
2022-11-10 20:10:05 -0600

    finished Lab 4

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 ## What Does Our App Do?
 
-This app allows users to vote on their favorite images of horned beasts.
+This app allows users to vote on their favorite images of horned beasts. By clicking on the images, the number of "likes" will increase and you will see a larger display of the title, image, and description.
+
+You may also use the form at the top of the page to filter the cards based on the number of horns each beast has. For example, selecting 2 horns and clicking the Submit button will only show the beasts with two horns.
 
 ## Time Estimate
 
@@ -30,9 +32,16 @@ This app allows users to vote on their favorite images of horned beasts.
 ### Feature #4: Modal
 
 * Estimate of time needed to complete: 3 hours
-* Start time: 2:00pm
+* Start time: 2pm
 * Finish time: 5pm
 * Actual time needed to complete: 2.5 hours
+
+### Feature #5: Form
+
+* Estimate of time needed to complete: 3 hours
+* Start time: 2pm
+* Finish time: 4pm
+* Actual time needed to complete: 2 hours
 
 Author: Jason Christopher
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -12,9 +12,10 @@ class Main extends React.Component {
     }
   }
 
+  // updates the "data" array in state with only the beasts with the requested number of horns
   handleSubmit = (event) => {
     event.preventDefault();
-    if(isNaN(this.state.requestedNumberOfHorns)) {
+    if (isNaN(this.state.requestedNumberOfHorns)) {
       this.setState({ newData: this.props.data });
     } else {
       let update = this.props.data.filter(beast => {
@@ -24,6 +25,7 @@ class Main extends React.Component {
     }
   }
 
+  // updates the requested number of horns in state
   handleChange = (event) => {
     event.preventDefault();
     this.setState({
@@ -33,6 +35,7 @@ class Main extends React.Component {
 
   render() {
 
+    // creates the cards based on the requested beast data in state
     let beastArray = this.state.newData.map(beast => {
       return <HornedBeast
         key={beast._id}
@@ -44,40 +47,32 @@ class Main extends React.Component {
         handleOpenModal={this.props.handleOpenModal}
       />
     });
-  
-    let hornsNumberArray = [];
 
-    let hornsOptions = this.props.data.map(val => {
-      if(!hornsNumberArray.includes(val.horns)) {
-        hornsNumberArray.push(val.horns);
-        return <option key = {val.horns}>{val.horns}</option>;
+    // makes the drop-down options for the form
+    let compare = [];
+    let hornsOptions = [];
+    this.props.data.forEach(beast => {
+      if (!compare.includes(beast.horns)) {
+        compare.push(beast.horns);
+        hornsOptions.push(
+          <option key={beast.horns}>{beast.horns}</option>
+        );
       }
-    });
+    })
 
-    // let compare = [];
-
-
-
+    //renders the actual form and cards
     return (
       <>
         <main>
           <Form id="form" onSubmit={this.handleSubmit}>
-            <fieldset>
-              <Form.Group className="mb-3">
-                <Form.Label id="formLabel" htmlFor="Select">
-                  Search by Number of Horns
-                </Form.Label>
-                <Form.Select id="Select" onChange={this.handleChange}>
-                  <option value="Number of Horns">Number of Horns</option>
-                  {hornsOptions}
-                </Form.Select>
-              </Form.Group>
-              <Button 
-                id="button" 
-                type="submit">
-                  Submit
-              </Button>
-            </fieldset>
+            <Form.Label id="formLabel" htmlFor="Select">
+              Search by Number of Horns
+            </Form.Label>
+            <Form.Select id="Select" onChange={this.handleChange}>
+              <option value="Number of Horns">Number of Horns</option>
+              {hornsOptions}
+            </Form.Select>
+            <Button id="button" type="submit">Submit</Button>
           </Form>
           <article>
             {beastArray}
